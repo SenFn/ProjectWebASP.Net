75d7571991fb8c2b4835cd7619f96a3ee69708f8
Sen <hoangnhan26212@gmail.com>
2021-07-06 17:12:09 +0700

    git push backup

---
+++ WebQuanAo/Controllers/gioHang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebQuanAo.Controllers
+{
+    public class GioHang
+    {
+        public int? id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string size { get; set; }
+        public decimal? price { get; set; }
+        public int count { get; set; }
+
+        public GioHang(int? id, string name, string url, string size, decimal? price, int count)
+        {
+            this.id = id;
+            this.name = name;
+            this.url = url;
+            this.size = size;
+            this.price = price;
+            this.count = count;
+        }
+
+        public override string ToString()
+        {
+            return id + "|" + name + "|" + url + "|" + size + "|" + price + "|" + count;
+        }
+    }
+}
---
+++ WebQuanAo/Models/product.cs
@@ -0,0 +1,45 @@
+namespace WebQuanAo.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("product")]
+    public partial class product
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public product()
+        {
+            cardInfo = new HashSet<cardInfo>();
+            productInfo = new HashSet<productInfo>();
+        }
+
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string name { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string srcImage { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal price { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string types { get; set; }
+
+        [StringLength(50)]
+        public string byHuman { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<cardInfo> cardInfo { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<productInfo> productInfo { get; set; }
+    }
+}
